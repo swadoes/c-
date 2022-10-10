bc1edac71514cca2627897ec6086976e324792f9
swadoes <pokemon21332@mail.ru>
2022-10-10 17:17:03 +0300

    change 3 work

--- homework 2/3/Program.cs
+++ homework 2/3/Program.cs
@@ -5,7 +5,10 @@ string input = Convert.ToString(anyNum);
 if (anyNum != 6 && anyNum != 7){
     Console.WriteLine("Рабочий день");
   
+}else if (anyNum>7){
+    Console.WriteLine("Это точно день недели?");
 }
+
  else{
      Console.WriteLine("этот день является выходным");
 }
