f9ea6c60a93a9a324c92578f74b25264daa8a044
swadoes <pokemon21332@mail.ru>
2022-10-10 17:30:19 +0300

    add homework 3

---
+++ homework 3/1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int len = number.Length;
+
+if (len == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - Палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - НЕ палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
+}
---
+++ homework 3/3/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int numb = ReadInt("Введите число N: ");
+
+for (int i = 1; i <= numb; i++)
+{ 
+    Console.Write($"{i*i*i} ");
+}
